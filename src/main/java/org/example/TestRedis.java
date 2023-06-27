2f86ee3b1882aaee76452afc979a09e4b4ec69a0
VitaliyDmitrienko <vddjack2008@yandex.ru>
2023-06-27 12:16:36 +0300

    1st.

---
+++ src/main/java/org/example/TestRedis.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Map;
+
+public class TestRedis {
+    public static void main(String[] args) {
+        Jedis jedis = new Jedis("localhost");
+        System.out.println(jedis.ping());
+
+        jedis.set("TestKey","TestValue");
+
+        System.out.println("Метод для проверки наличия индекса");
+        System.out.println(jedis.exists("TestKey"));
+        System.out.println(jedis.exists("TestValue"));
+        System.out.println(jedis.exists("FalseTestKey"));
+        System.out.println ("||-----||\n");
+
+        System.out.println("Метод добавления словаря");
+        System.out.println(jedis.hset("TestSetKey", "Field", "Value"));
+        System.out.println(jedis.hset("TestSetKey", "another", "testvalue"));
+        //Вывод в результат в одного значения
+        System.out.println(jedis.hget("TestSetKey", "another"));
+
+        //Карта всех значений
+        Map<String, String> fields = jedis.hgetAll("TestSetKey");
+        for (String field:fields.keySet()){
+            System.out.println(field);
+        }
+        //Вывод значения конкретно поля уже из полученного множества
+        System.out.println(fields.get("another"));
+        //Удаляем карту
+        System.out.println(jedis.del("TestSetKey"));
+    }
+}
